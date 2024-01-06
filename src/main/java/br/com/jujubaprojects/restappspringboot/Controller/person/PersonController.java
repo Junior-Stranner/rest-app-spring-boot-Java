9b89c48e2ced871819205407d1085a6381742812
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-06 16:17:47 -0300

    add Person Service

--- src/main/java/br/com/jujubaprojects/restappspringboot/Controller/person/PersonController.java
+++ src/main/java/br/com/jujubaprojects/restappspringboot/Controller/person/PersonController.java
@@ -1,8 +1,12 @@
 package br.com.jujubaprojects.restappspringboot.Controller.person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.jujubaprojects.restappspringboot.Model.person.Person;
 import br.com.jujubaprojects.restappspringboot.Service.person.PersonService;
 
 @RestController
@@ -11,5 +15,4 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    
 }
