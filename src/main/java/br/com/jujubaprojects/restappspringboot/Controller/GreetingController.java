0fd3bb933cecf2de87c9d3fef4a4757af3df141f
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-07 17:47:29 -0300

    remove model greeting

--- src/main/java/br/com/jujubaprojects/restappspringboot/Controller/GreetingController.java
+++ src/main/java/br/com/jujubaprojects/restappspringboot/Controller/GreetingController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.jujubaprojects.restappspringboot.Model.Greeting;
 
 @RestController
 public class GreetingController {
@@ -15,8 +14,9 @@ public class GreetingController {
     private static  final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")  String name){
-        return new Greeting(counter.incrementAndGet() ,String.format(template, name));
+    public String greeting(@RequestParam(value = "name", defaultValue = "World")  String name){
+         return new String (String.format(template, name));
+       // return new Greeting(counter.incrementAndGet() ,String.format(template, name));
 
     }
 }
