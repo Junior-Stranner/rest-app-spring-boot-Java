6ee5968eba596f8ecf90098df948208ca824081e
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-08 15:04:38 -0300

    Get,POST,PUT,DELETE working perfectly

--- src/main/java/br/com/jujubaprojects/restappspringboot/Controller/person/PersonController.java
+++ src/main/java/br/com/jujubaprojects/restappspringboot/Controller/person/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,7 +47,8 @@ public class PersonController {
 	
 	
 	@DeleteMapping(value = "/{id}")
-	public void delete(@PathVariable(value = "id") Long id) {
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		personService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
