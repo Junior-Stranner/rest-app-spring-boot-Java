b2feb17b387a928ec8cc43b5656ab8b5b08e3dc0
Junior-Stranner <JuniorStranner@gmail.com>
2025-04-05 15:04:49 -0300

    add section 8

---
+++ 8_LogsInSpringBoot/version-2025-Ju/src/main/java/br/com/judev/version2025ju/controllers/PersonController.java
@@ -0,0 +1,54 @@
+package br.com.judev.version2025ju.controllers;
+
+import br.com.judev.version2025ju.controllers.documentation.PersonDocumentationController;
+import br.com.judev.version2025ju.model.Person;
+import br.com.judev.version2025ju.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController implements PersonDocumentationController {
+    @Autowired
+    private PersonService service;
+    // private PersonServices service = new PersonServices();
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
