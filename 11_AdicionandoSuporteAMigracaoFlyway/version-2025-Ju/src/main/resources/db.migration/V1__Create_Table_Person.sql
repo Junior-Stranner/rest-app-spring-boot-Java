3d522ddf444f9e1a94e86a0635d1316293008efd
Junior-Stranner <JuniorStranner@gmail.com>
2025-04-18 15:16:34 -0300

    add another section

---
+++ 11_AdicionandoSuporteAMigracaoFlyway/version-2025-Ju/src/main/resources/db.migration/V1__Create_Table_Person.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE public.person (
+                               id bigint NOT NULL,
+                               address character varying(100) NOT NULL,
+                               first_name character varying(80) NOT NULL,
+                               gender character varying(6) NOT NULL,
+                               last_name character varying(80) NOT NULL
+);
+
---
+++ 11_AdicionandoSuporteAMigracaoFlyway/version-2025-Ju/src/main/resources/db.migration/V2__Populate_table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (1, 'Laguna - Santa Catarina - Brasil', 'Ayrton', 'female', 'Senna');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (2, 'Laguna - Santa Catarina - Brasil', 'Ayrton', 'female', 'Senna');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (3, 'São Paulo', 'Ayrton', 'male', 'Senna');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (4, 'São Paulo', 'Ayrton', 'male', 'Senna');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (5, 'SC', 'Junior', 'male', 'Bomber');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (6, 'SC', 'Maikon', 'male', 'Bomber');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (7, 'SC', 'Gissele', 'male', 'Kibllmer');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (8, 'SC', 'Vanessa', 'female', 'Kibllmer');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (9, 'SC', 'João', 'male', 'Gomes');
+INSERT INTO public.person (id, address, first_name, gender, last_name) VALUES (10, 'SC', 'Bubu', 'female', 'Ferrari');
+
